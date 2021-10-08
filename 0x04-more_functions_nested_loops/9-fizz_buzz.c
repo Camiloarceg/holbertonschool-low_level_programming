96caf0da1ee07dad7155b39566f4898054222966
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-10-08 18:43:06 +0000

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the number and fizzbuzz
+ *
+ * Return: void.
+ */
+int main(void)
+{
+	int i, indicador;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		indicador = 0;
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			indicador = 1;
+			printf("FizzBuzz");
+		}
+		if ((i % 3) == 0)
+		{
+			indicador = 1;
+			printf("Fizz");
+		}
+		if ((i % 5) == 0)
+		{
+			indicador = 1;
+			printf("Buzz");
+		}
+		if (indicador == 0)
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
