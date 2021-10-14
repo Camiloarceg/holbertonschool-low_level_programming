721b70414197759f15e1521ee63db7fd8a855433
Holberton <3773@holbertonschool.com>
2021-10-14 15:14:46 -0500

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,27 +7,35 @@
  */
 char *cap_string(char *p)
 {
-	count += 1;
-    if (!((*p >= 97 && *p <= 122)||(*p >= 65 && *p <= 90)))
-    {
-      cap = 1;
-    }
-    p++;
-    if ((cap == 1) && ((*p >= 97 && *p <= 122)))
-    {
-      *p = *p - 32;
-      cap = 0;
-    }
-    if ((cap == 1) && (*p >= 65 && *p <= 90))
-    {
-      cap = 0;  
-    }
-  }
-  while (count > 0)
-    {
-        p--;
-        count--;
-    }
-  
-  return (p);
+	int cap = 0;
+	int count = 0;
+	int i;
+	int alert[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (*p != '\0')
+	{
+		for (i = 0 ; i <= 13 ; i++)
+		{
+			if (*p == alert[i])
+				cap = 1;
+		}
+		p++;
+		if ((cap == 1) && ((*p >= 97 && *p <= 122)))
+		{
+			*p = *p - 32;
+			cap = 0;
+		}
+		if ((cap == 1) && !(*p >= 97 && *p <= 122))
+		{
+			cap = 0;  
+		}
+		count += 1;
+	}
+	while (count > 0)
+	{
+		p--;
+		count--;
+	}
+
+	return (p);
 }
