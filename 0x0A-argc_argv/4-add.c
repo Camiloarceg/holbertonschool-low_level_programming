fba8824853bb60513129193fe8c16978ea9d9e98
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-10-22 19:57:09 +0000

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,25 +9,26 @@
  */
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int sum = 0, i, m;
+	char *s;
 
-	if (argc < 1)
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1 ; i < argc ; i++)
-	{
-		if (*argv[i] != '0')
+		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0)
+			s = argv[i];
+			for (m = 0; s[m]; m++)
 			{
-				printf("Error\n");
-				return (1);
+				if (s[m] < 48 || s[m] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			sum += atoi(argv[i]);
 		}
 	}
+	for (i = 1; i < argc; i++)
+		sum += atoi(argv[i]);
 	printf("%d\n", sum);
 	return (0);
 }
