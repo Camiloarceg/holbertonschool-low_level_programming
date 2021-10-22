2980b43c8dfb78788e0c48305ebdec99e83400c5
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-10-22 23:28:32 +0000

    have been better days

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer
+ *@s: the string.
+ *
+ * Return: Positive or negative integer
+ */
+
+int _atoi(char *s)
+{
+	(void)s;
+	return (0);
+}
