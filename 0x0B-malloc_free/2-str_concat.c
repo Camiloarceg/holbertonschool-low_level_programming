04f66949228110c33b819b0aa121d8893e9762f9
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-10-26 19:20:38 +0000

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,25 +24,44 @@ int elmayor(int len1, int len2)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2;
+	int i, len1 = 0, len2 = 0;
 	char *p;
 
-	for (i = 0 ; s1[i] != '\0' ; i++)
+	if (s1 == 0 && s2 == 0)
+		return (0);
+	if (s1 == 0)
 	{
+		len1 = 0;
+		len2 += 1;
 	}
-	len1 = i;
-	for (i = 0 ; s2[i] != '\0' ; i++)
+	else
 	{
+		for (i = 0 ; s1[i] != '\0' ; i++)
+		{
+		}
+		len1 += i;
 	}
-	len2 = i;
+	if (s2 == 0)
+	{
+		len2 = 0;
+		len1 += 1;
+	}
+	else
+	{
+		for (i = 0 ; s2[i] != '\0' ; i++)
+		{
+		}
+		len2 += i;
+	}
 	p = (char *)malloc((len1 + len2) * sizeof(char) + 1);
 	if (p == 0)
 		return (0);
 	for (i = 0 ; i < elmayor(len1, len2) ; i++)
 	{
-		if (i < len1)
+		if (i < len1 && len1 > 0)
 			p[i] = s1[i];
-		p[len1 + i] = s2[i];
+		if (i < len2 && len2 > 0)
+			p[len1 + i] = s2[i];
 	}
 	return (p);
 }
