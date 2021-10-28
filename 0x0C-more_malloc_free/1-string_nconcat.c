34548cf111012ffd210d6d200fd97c700c1997f2
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-10-28 19:11:40 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,51 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 /**
- * string_nconcat - concatenates two strings.
- *@s1: 1 string
- *@s2: 2 string
- *@n: n first bytes of s2
- *
- * Return: a pointer to the new allocated string.
+ *  * string_nconcat - function that concatenates two strings.
+ *   * @s1: pointer char
+ *    * @s2: pointer char
+ *     * @n: unsigned int
+ *      *
+ *       * Return: void
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, i, j;
-	char *p;
+	char *concat;
+	unsigned int l1, l2, c, c2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (len1 = 0 ; s1[len1] != '\0' ; len1++)
+	/*leer el tamano de las cadenas*/
+	for (l1 = 0; s1[l1] != '\0'; l1++)
 	{}
-	for (len2 = 0 ; s2[len2] != '\0' ; len2++)
+	for (l2 = 0; s2[l2] != '\0'; l2++)
 	{}
-	if (n < len2)
-		len2 = n;
-	p = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (p != NULL)
+	/*(CONDICION) si n es mayor o igual a la longitud de s2 se muestra todo s2*/
+	/*Necesito es que no se muestre todo*/
+	if (n < l2)
 	{
-		for (i = 0 ; s1[i] != '\0' ; i++)
-			p[i] = s1[i];
-		for (j = 0 ; j < len2 ; j++)
-			p[i + j] = s2[j];
-		p[i + j] = '\0';
-		return (p);
+		l2 = n;
+	}
+	/*asigno tamano al concatenado*/
+	concat = malloc(sizeof(char) * (l1 + l2 + 1)); /* +1 para obtener nulo*/
+
+	if (concat != NULL)
+	{
+		/*escribo las cadenas en el nuevo array*/
+		for (c = 0; s1[c] != '\0'; c++)
+		{
+			concat[c] = s1[c];
+		}
+		/*Aumento c para q no se sobre escriba c2 en c*/
+		for (c2 = 0; c2 < l2; c2++)
+		{
+			concat[c + c2] = s2[c2];
+		}
+		concat[c + c2] = '\0';
+		return (concat);
 	}
 	else
 		return (NULL);
