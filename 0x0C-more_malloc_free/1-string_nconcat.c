ce07311f503ba8d268d9ed05af5039a16dd6ead1
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-10-28 18:45:38 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -47,8 +47,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			p[i] = s1[i];
 		if (i < len2)
 			p[len1 + i] = s2[i];
-		if (i == len2)
-			p[len1 + i] = '\0';
 	}
+	p[len1 + i] = '\0';
 	return (p);
 }
