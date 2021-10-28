f68e0e9be83dd6129a7fb7c5d4d38c41d331476b
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-10-28 19:07:08 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * elmayor - return the greater int.
- *@len1: int 1
- *@len2: int 2
- *
- * Return: the greater
- */
-unsigned int elmayor(int len1, int len2)
-{
-	if (len1 > len2)
-		return (len1 + 1);
-	else
-		return (len2 + 1);
-	return (0);
-}
-/**
  * string_nconcat - concatenates two strings.
  *@s1: 1 string
  *@s2: 2 string
@@ -38,10 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{}
 	if (n < len2)
 		len2 = n;
-	p = malloc(sizeof(*p) * (len1 + len2 + 1));
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (p != NULL)
 	{
-		for (i = 0 ; i != '\0' ; i++)
+		for (i = 0 ; s1[i] != '\0' ; i++)
 			p[i] = s1[i];
 		for (j = 0 ; j < len2 ; j++)
 			p[i + j] = s2[j];
