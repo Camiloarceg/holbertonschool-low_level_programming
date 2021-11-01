9435f02cb4713a803c598c743f2664d93aefad20
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-11-01 21:50:08 +0000

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,26 +50,36 @@ int _strlen(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
-	/*hacer espacio en memoria*/
+
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
-	{
-		free(my_dog);
 		return (NULL);
-	}
-	my_dog->name = NULL;
-	my_dog->owner = NULL;
-	my_dog->name = malloc((_strlen(name) + 1) * sizeof(char));
-	my_dog->owner = malloc((_strlen(owner) + 1) * sizeof(char));
-	if (my_dog->owner == NULL || my_dog->name == NULL)
+	if (name == NULL)
+		my_dog->name = NULL;
+	else
 	{
-		free(my_dog->name);
-		free(my_dog->owner);
-		free(my_dog);
-		return (NULL);
+		my_dog->name = (char *) malloc(_strlen(name) + 1);
+		if (my_dog->name == NULL)
+		{
+			free(my_dog);
+			return (NULL);
+		}
+		_strcpy(my_dog->name, name);
 	}
-	my_dog->owner = _strcpy(my_dog->owner, owner);
-	my_dog->name = _strcpy(my_dog->name, name);
+	if (owner == NULL)
+		my_dog->owner = NULL;
+	else
+	{
+		my_dog->owner = (char *) malloc(_strlen(owner) + 1);
+		if (my_dog->owner == NULL)
+		{
+			free(my_dog->name);
+			free(my_dog);
+			return (NULL);
+		}
+		_strcpy(my_dog->owner, owner);
+	}
 	my_dog->age = age;
 	return (my_dog);
 }
+
