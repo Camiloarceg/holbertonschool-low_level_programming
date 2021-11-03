86781b571d8f5371f7e2f1b54baf0d438f47e383
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-11-03 17:47:30 +0000

    :v

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,6 @@
 #ifndef FPTR_H
 #define FPTR_H
-typedef struct Size_t{
-	int size;
-}size_t;
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
