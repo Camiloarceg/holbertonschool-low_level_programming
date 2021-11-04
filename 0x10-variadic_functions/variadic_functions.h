4f97931672fc6869121292eab7b39d4f2effc341
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-11-04 22:27:53 +0000

    acc header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef VF
 #define VF
 #include <stdarg.h>
+/**
+  struct formats - Struct formats
+ *
+ * @format: The format
+ * @func: The function associated
+ */
+typedef struct Formats
+{
+
+	char *format;
+	void (*func)();
+}format;
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
