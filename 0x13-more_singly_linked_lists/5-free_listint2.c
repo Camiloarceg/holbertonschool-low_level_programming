aa37d4dc790f1a6697e959afb41689a461326bde
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-11-15 21:00:38 +0000

    add task 5 prechecker

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 -  frees a listint_t list.
+ * @head: pointer that points to a pointer to a node.
+ *
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	while (tmp != NULL)
+	{
+		*head = (*head)->next;
+		free(tmp);
+		tmp = *head;
+	}
+	*head = NULL;
+}
+
