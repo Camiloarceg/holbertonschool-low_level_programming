0fc058f5514202d466d9709f5e0d3d8e2a9557a7
Juan_Camilo_Arce <camiloarceg@gmail.com>
2021-11-16 12:44:01 +0000

    fixed psible fail for task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,30 +12,31 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *tmp = *head;
 	listint_t *new_n;
 	unsigned int len = 0;
-	listint_t *prev;
 
 	new_n = malloc(sizeof(*new_n));
 	if (new_n == NULL)
-		return (NULL);
-
-	while (tmp != NULL)
 	{
-		tmp = tmp->next;
-		len += 1;
-	}
-	if (idx > len)
+		free(new_n);
 		return (NULL);
-	for (tmp = *head ; idx > 0 ; idx--)
-	{
-		if (idx == 1)
-			prev = tmp;
-		tmp = tmp->next;
 	}
-	new_n->next = tmp;
 	new_n->n = n;
-	if (idx > 0)
-		prev->next = new_n;
-	else
+	if (idx == 0)
+	{
+		new_n->next = *head;
 		*head = new_n;
+		return (new_n);
+	}
+	while (len != (idx - 1))
+	{
+		if (tmp == NULL)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		tmp = tmp->next;
+		len++;
+	}
+	new_n->next = tmp->next;
+	tmp->next = new_n;
 	return (new_n);
 }
