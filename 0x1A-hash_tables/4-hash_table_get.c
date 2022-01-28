9bcdfd78d4ba3bf5e72010133869792d3957b6e9
Camiloarceg <camiloarceg@gmail.com>
2022-01-28 14:09:00 -0500

    add

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,9 +1,10 @@
 #include "hash_tables.h"
 /**
  * hash_table_get -  retrieves a value associated with a key.
- * @ht: the hash table. 
- * @key: the key to look for. 
- * Return: he value associated with the element, or NULL if key couldn’t be found. 
+ * @ht: the hash table.
+ * @key: the key to look for.
+ * Return: he value associated with the element, or
+ *  NULL if key couldn’t be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
